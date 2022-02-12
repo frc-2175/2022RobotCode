ddf7ea443153dee62ecb37e58cbc888c028c97b4
Giga Bowser <45986823+Giga-Bowser@users.noreply.github.com>
2022-02-12 14:13:29 -0600

    Write drive code.

--- src/lua/robot.lua
+++ src/lua/robot.lua
@@ -1,7 +1,6 @@
 require("subsystems.cargo")
 require("subsystems.climber")
 require("subsystems.drivetrain")
-require("wpilib.dashboard")
 require("utils.logger")
 require("utils.blendeddrive")
 require("utils.purepursuit")
@@ -13,21 +12,32 @@ function Robot.robotInit()
 	rightStick = Joystick:new(1)
 	gamepad = Joystick:new(2)
 	-- serbo = Servo:new(1111)
-	chooser = SendableChooser:new()
-	chooser:putChooser({
-		{name = "Test 1", value = "they're selecting test 1"},
-		{name = "Test 2", value = "they're selecting test 2"},
-	})
-	pathy = makePath(false, 0, Vector:new(0,0), {makeLinePathSegment(10)})
-	purepursuity = PurePursuit:new(pathy, 0, 0, 0)
+
+	leftMotor = TalonFX:new(0)
+	rightMotor = TalonFX:new(1)
+	
+	leftFollower = TalonFX:new(2)
+	leftFollower:follow(leftMotor)
+	leftFollower:setInverted(CTRETalonFXInvertType.FollowMaster)
+	rightFollower = TalonFX:new(3)
+	rightFollower:follow(rightMotor)
+	rightFollower:setInverted(CTRETalonFXInvertType.FollowMaster)
+
+	robotDrive = DifferentialDrive:new(leftMotor, rightMotor)
 end
 
 function Robot.autonomousPeriodic()
-	turnvalue = purepursuity:run()
-	getBlendedMotorValues(.5,turnvalue)
 end
 
 function Robot.teleopPeriodic()
+	-- joystick driving
+	robotDrive:arcadeDrive(
+        leftStick:getY(),
+        rightStick:getX(),
+		false
+    )
+
+
 	if rightStick:getTriggerHeld() then
 		Intake:runIn()
 	else
