e298eb3e0edd2641104c97e4876e70c3ee23680e
Giga Bowser <45986823+Giga-Bowser@users.noreply.github.com>
2022-02-11 18:45:06 -0600

    Fix.

--- tools/pathMaker/script.js
+++ tools/pathMaker/script.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+disableFriendlyErrors = true;
 
 class Vector {
 	constructor(x, y) {
@@ -6,16 +7,47 @@ class Vector {
 		this.y = y;
 	}
 
-	convToScreenCoords() {
-		return new Vector(xImageCenter + this.x / pixelToInchRatio, yImageCenter + this.y / pixelToInchRatio * -1);
-	}
-
-	convToFieldCoords() {
-		return new Vector(round((xImageCenter - this.x) * pixelToInchRatio) * -1, round((yImageCenter - this.y) * pixelToInchRatio));
-	}
-
 	distTo(vector) {
 		return dist(this.x, this.y, vector.x, vector.y);
+	}
+
+	add(vector) {
+		vector = typeof vector === "number" ? new Vector(vector, vector) : vector;
+		return new Vector(this.x + vector.x, this.y + vector.y);
+	}
+
+	sub(vector) {
+		vector = typeof vector === "number" ? new Vector(vector, vector) : vector;
+		return new Vector(this.x - vector.x, this.y - vector.y);
+	}
+
+	div(vector) {
+		vector = typeof vector === "number" ? new Vector(vector, vector) : vector;
+		return new Vector(this.x / vector.x, this.y / vector.y);
+	}
+	
+	mul(vector) {
+		vector = typeof vector === "number" ? new Vector(vector, vector) : vector;
+		return new Vector(this.x * vector.x, this.y * vector.y);
+	}
+
+	toScreen() {
+		return new Vector(
+			imageCenter.x + this.x / pixelToInchRatio,
+			imageCenter.y + this.y / -pixelToInchRatio
+		);
+	}
+
+	toField() {
+		return new Vector(
+			-round((imageCenter.x - this.x) * pixelToInchRatio),
+			round((imageCenter.y - this.y) * pixelToInchRatio)
+		);
+	}
+
+	*[Symbol.iterator] () {
+		yield this.x;
+		yield this.y;
 	}
 }
 
@@ -23,18 +55,17 @@ let img;
 let dataChangedSinceSave = true;
 let pixelToInchRatio = null;
 let pointList = [];
-let lineVectorList = [];
+let lineVectors = [];
 let lineVectorHasChanged = false;
 const stepIncrement = 1;
 let pointFile;
-let xImageCenter = null;
-let yImageCenter = null;
-const visualizeNPoints = 16;
-let currentSegment = 1;
-let closestLineIndex;
+let imageCenter = null;
+const visualizeNPoints = 10;
 let closestPoint;
 let mouseVector;
+let fieldMouse;
 const triggerPointList = [];
+
 const colorList = [
 	{
 		"name": "gray",
@@ -75,27 +106,19 @@ function preload() {
 	img = loadImage("https://firebasestorage.googleapis.com/v0/b/pathmakerviewer.appspot.com/o/rapidreactfield.png?alt=media&token=8cf9f0e0-b56f-49b6-941b-c9240db1a2d7");
 }
 
+// eslint-disable-next-line no-unused-vars
 function setup() {
 	createCanvas(windowWidth, (windowWidth * 0.58));
-	xImageCenter = width / 2;
-	yImageCenter = height / 2;
+	imageCenter = new Vector(width / 2, height / 2);
 	pixelToInchRatio = 1.37 / (width / (2987 / 5));
 	textSize(15);
 }
 
 function createNewLineVector(vector) {
-	lineVectorList.push(vector);
+	lineVectors.push(vector);
 	lineVectorHasChanged = true;
 	dataChangedSinceSave = true;
 	updateTitle(pointFile);
-}
-
-function convXToScreen(x) {
-	return xImageCenter + x / pixelToInchRatio;
-}
-
-function convYToScreen(y) {
-	return yImageCenter + y / pixelToInchRatio * -1;
 }
 
 function createNewPoint(vector) {
@@ -104,24 +127,22 @@ function createNewPoint(vector) {
 }
 
 function removeLastLineVector() {
-	if (1 < lineVectorList.length) {
-		const previousPoint = lineVectorList[lineVectorList.length - 2];
-		const currentPoint = lineVectorList[lineVectorList.length - 1];
+	if (lineVectors.length > 1) {
+		const previousPoint = lineVectors[lineVectors.length - 2];
+		const currentPoint = lineVectors[lineVectors.length - 1];
 		const length = ceil(previousPoint.distTo(currentPoint) / stepIncrement);
 		for (let i = 0; i < length; i++) {
 			pointList.pop();
 		}
-		currentSegment -= 1;
 	}
 	dataChangedSinceSave = true;
-	lineVectorList.pop();
+	lineVectors.pop();
 	updateTitle(pointFile);
 }
 
 // eslint-disable-next-line no-unused-vars
-function mousePressed() {
-	const mouseVector = new Vector(mouseX, mouseY);
-	createNewLineVector(mouseVector.convToFieldCoords());
+function mouseClicked() {
+	createNewLineVector((new Vector(mouseX, mouseY)).toField());
 }
 
 document.addEventListener("keydown", function (e) {
@@ -151,7 +172,7 @@ function updateTitle(handle) {
 		}
 	}
 	catch {
-		console.error("File not selected yet.");
+		//console.error("File not selected yet.");
 	}
 }
 
@@ -188,14 +209,14 @@ function savePoints() {
 	if (pointFile == null) {
 		getNewFileHandle().then(result => {
 			updateTitle(result);
-			writeFileToDisk(result, JSON.stringify({ points: pointList, lineVectors: lineVectorList }));
+			writeFileToDisk(result, JSON.stringify({ points: pointList, "lineVectors": lineVectors }));
 			console.log(pointList);
 			pointFile = result;
 		});
 	}
 	else {
 		updateTitle(pointFile);
-		writeFileToDisk(pointFile, JSON.stringify({ points: pointList, lineVectors: lineVectorList }));
+		writeFileToDisk(pointFile, JSON.stringify({ points: pointList, "lineVectors": lineVectors }));
 	}
 }
 
@@ -204,77 +225,57 @@ async function openPoints() {
 	const file = await fileHandle.getFile();
 	const contents = JSON.parse(await file.text());
 	console.log(contents);
-	lineVectorList = [];
-	pointList = [];
+	lineVectors = [];
+	pointList = [];	
 	contents["lineVectors"].forEach((item) => {
-		lineVectorList.push(new Vector(item["x"], item["y"]));
+		lineVectors.push(new Vector(item["x"], item["y"]));
 	});
 	contents["points"].forEach((item) => {
 		pointList.push(new Vector(item["x"], item["y"]));
 	});
 }
 
-
-// vector 1, vector 2, mouse vector
-function getDistanceToLine(v1, v2, m) {
-	const numer = ((v2.x - v1.x) * (v1.y - m.y)) - ((v1.x - m.x) * (v2.y - v1.y));
-	const denom = Math.sqrt(((v2.x - v1.x) ** 2) + ((v2.y - v1.y) ** 2));
-	
-	return Math.abs(numer) / denom;
-}
-
-
 function getClosestPointOnLines(pXy, aXys) {
-	let minDist, fTo, fFrom, x, y, i, dist;
+	let fFrom, fTo, i, dist, minDist, x, y;
 
-	if (aXys.length > 1) {
-		for (let n = 1; n < aXys.length; n++) {
-			if (aXys[n].x !== aXys[n - 1].x) {
-				const a = (aXys[n].y - aXys[n - 1].y) / (aXys[n].x - aXys[n - 1].x);
-				const b = aXys[n].y - a * aXys[n].x;
-				dist = Math.abs(a * pXy.x + b - pXy.y) / Math.sqrt(a * a + 1);
-			} else {
-				dist = Math.abs(pXy.x - aXys[n].x);
-			}
+	for (let n = 1; n < aXys.length; n++) {
+		if (aXys[n].x !== aXys[n - 1].x) {
+			const a = (aXys[n].y - aXys[n - 1].y) / (aXys[n].x - aXys[n - 1].x);
+			const b = aXys[n].y - a * aXys[n].x;
+			dist = Math.abs(a * pXy.x + b - pXy.y) / Math.sqrt(a * a + 1);
+		} else {
+			dist = Math.abs(pXy.x - aXys[n].x);
+		}
 
-			// length^2 of line segment 
-			const rl2 = Math.pow(aXys[n].y - aXys[n - 1].y, 2) + Math.pow(aXys[n].x - aXys[n - 1].x, 2);
+		// length of line segment 
+		const rl = aXys[n].distTo(aXys[n - 1]);
+		// distance of pt to end line segment
+		const ln = pXy.distTo(aXys[n]);
+		// distance of pt to begin line segment
+		const lnm1 = pXy.distTo(aXys[n - 1]);
+		// calculated length of line segment
+		const calcrl = ln + lnm1 - dist;
 
-			// distance^2 of pt to end line segment
-			const ln2 = Math.pow(aXys[n].y - pXy.y, 2) + Math.pow(aXys[n].x - pXy.x, 2);
+		// redefine minimum distance to line segment (not infinite line) if necessary
+		if (calcrl > rl) dist = Math.min(ln, lnm1);
 
-			// distance^2 of pt to begin line segment
-			const lnm12 = Math.pow(aXys[n - 1].y - pXy.y, 2) + Math.pow(aXys[n - 1].x - pXy.x, 2);
-
-			// minimum distance^2 of pt to infinite line
-			const dist2 = Math.pow(dist, 2);
-
-			// calculated length^2 of line segment
-			const calcrl2 = ln2 - dist2 + lnm12 - dist2;
-
-			// redefine minimum distance to line segment (not infinite line) if necessary
-			if (calcrl2 > rl2)
-				dist = Math.sqrt(Math.min(ln2, lnm12));
-
-			if ((minDist == null) || (minDist > dist)) {
-				if (calcrl2 > rl2) {
-					if (lnm12 < ln2) {
-						fTo = 0;//nearer to previous point
-						fFrom = 1;
-					}
-					else {
-						fFrom = 0;//nearer to current point
-						fTo = 1;
-					}
+		if (minDist == null || minDist > dist) {
+			if (calcrl > rl) {
+				if (lnm1 < ln) {
+					fTo = 0;//nearer to previous point
+					fFrom = 1;
 				}
 				else {
-					// perpendicular from point intersects line segment
-					fTo = ((Math.sqrt(lnm12 - dist2)) / Math.sqrt(rl2));
-					fFrom = ((Math.sqrt(ln2 - dist2)) / Math.sqrt(rl2));
+					fFrom = 0;//nearer to current point
+					fTo = 1;
 				}
-				minDist = dist;
-				i = n;
+			} else {
+				// perpendicular from point intersects line segment
+				fTo = Math.sqrt(lnm1 ** 2 - dist ** 2) / rl;
+				fFrom = Math.sqrt(ln ** 2 - dist ** 2) / rl;
 			}
+			minDist = dist;
+			i = n;
 		}
 
 		const dx = aXys[i - 1].x - aXys[i].x;
@@ -285,22 +286,20 @@ function getClosestPointOnLines(pXy, aXys) {
 
 	}
 
-	return { "x": x, "y": y, "i": i, "fTo": fTo, "fFrom": fFrom };
+	return { "vector": new Vector(x, y), "i": i, "fTo": fTo, "fFrom": fFrom };
 }
 
 function createTriggerPoint(point, name, code, color) {
-	const dist = 0;
-	let previousPoint = lineVectorList[0];
-	lineVectorList.forEach((item, index) => {
-		if (0 < index) {
+	let previousPoint = lineVectors[0];
+	lineVectors.forEach((item, index) => {
+		if (index > 0) {
 			console.log(previousPoint.distTo(item));
 			previousPoint = item;
 		}
 	});
-	if (closestPoint.distTo(mouseVector.convToFieldCoords()) < 50) {
+	if (closestPoint.distTo(mouseVector.toField()) < 50) {
 		triggerPointList.push({"vector": point, "name": name, "code": code, "color": color});
 	}
-	console.log(dist);
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -313,11 +312,9 @@ function keyPressed() {
 // eslint-disable-next-line no-unused-vars
 function windowResized() {
 	resizeCanvas(windowWidth, (windowWidth * 0.58));
-	xImageCenter = width / 2;
-	yImageCenter = height / 2;
+	imageCenter = new Vector(width / 2, height / 2);
 	pixelToInchRatio = 1.37 / (width / (2987 / 5));
 }
-
 
 function updateHTML() {
 	if (document.readyState === "ready" || document.readyState === "complete") {
@@ -334,113 +331,85 @@ function updateHTML() {
 	}
 }
 
+// eslint-disable-next-line no-unused-vars
 function draw() {
 	updateHTML();
 	mouseVector = new Vector(mouseX, mouseY);
-	const xCoord = mouseVector.convToFieldCoords().x;
-	const yCoord = mouseVector.convToFieldCoords().y;
-	let closestLineDist = Number.MAX_SAFE_INTEGER;
+	fieldMouse = mouseVector.toField();
 
 	background(220);
 	strokeWeight(0);
 	image(img, 0, 0, width, height);
+	fill(255);
 	text("Screen coordinates: " + String(round(mouseX) + ", " + round(mouseY)), 10, 20);
-	text("Field coordinates: " + xCoord + ", " + yCoord, 10, 40);
-
-	if (lineVectorList.length > 1) {
-		const currentLine = [lineVectorList[currentSegment - 1], lineVectorList[currentSegment]];
-		
-				
-		lineVectorList.forEach((value, index) => {
-			if (index > 0) {
-				const prev = lineVectorList[index - 1];
-				const curr = value;
-				const lineDist = getDistanceToLine(curr, prev, mouseVector.convToFieldCoords());
-				if (lineDist < closestLineDist) {
-					console.log("changed closest!");
-					closestLineDist = lineDist;
-					closestLineIndex = index;
-				}
-			}
-		});
-
-		currentSegment = closestLineIndex;
-		console.log(closestLineIndex);
-
-		closestPoint = new Vector(
-			getClosestPointOnLines(mouseVector.convToFieldCoords(), currentLine).x, 
-			getClosestPointOnLines(mouseVector.convToFieldCoords(), currentLine).y
-		);
-		strokeWeight(12);
-		stroke(200);
-		point(closestPoint.convToScreenCoords().x, closestPoint.convToScreenCoords().y);
-		stroke(0);
-	}
-
-	else {
-		const currentLine = null;
-	}
+	text("Field coordinates: " + fieldMouse.x + ", " + fieldMouse.y, 10, 40);
 
 	strokeWeight(1);
 
-	if (lineVectorList.length === 1) {
-		stroke(200);
-		line(convXToScreen(lineVectorList[0].x), convYToScreen(lineVectorList[0].y), mouseX, mouseY);
-	} else if (lineVectorList.length > 0) {
-		lineVectorList.forEach((item, index) => {
-			if (index !== 0) {
-				const previousPoint = lineVectorList[index - 1];
-				const currentPoint = item;
-				let pointDrawCount = 0;
-
-				pointList.forEach((item) => {
-					if (pointDrawCount % visualizeNPoints === 0) {
-						strokeWeight(5);
-						point(item.convToScreenCoords().x, item.convToScreenCoords().y);
-					}
-					pointDrawCount += 1;
-				});
-
-				strokeWeight(2);
-				line(convXToScreen(previousPoint.x), convYToScreen(previousPoint.y), convXToScreen(currentPoint.x), convYToScreen(currentPoint.y));
-			}
-
-			if (index === lineVectorList.length - 1) {
-				stroke(200);
-				line(convXToScreen(lineVectorList[lineVectorList.length - 1].x), convYToScreen(lineVectorList[lineVectorList.length - 1].y), mouseX, mouseY);
-			}
-		});
-
-		stroke(0);
-		strokeWeight(10);
-		triggerPointList.forEach((item) => {
-			stroke(0);
-			strokeWeight(18);
-			point(item["vector"].convToScreenCoords().x, item["vector"].convToScreenCoords().y);
-			strokeWeight(10);
-			stroke("#" + item["color"]);
-			point(item["vector"].convToScreenCoords().x, item["vector"].convToScreenCoords().y);
-		});
-		stroke(0);
-
-		const previousPoint = lineVectorList[lineVectorList.length - 2];
-		const currentPoint = lineVectorList[lineVectorList.length - 1];
+	if (lineVectors.length === 1) {
+		stroke(150);
+		line(...lineVectors[0].toScreen(), ...mouseVector);
+	} else if (lineVectors.length > 0) {
+		const previousPoint = lineVectors[lineVectors.length - 2];
+		const currentPoint = lineVectors[lineVectors.length - 1];
 		const length = previousPoint.distTo(currentPoint);
-		const xStepSize = ((currentPoint.x - previousPoint.x) / length);
-		const yStepSize = ((currentPoint.y - previousPoint.y) / length);
+		const stepSize = (currentPoint.sub(previousPoint)).div(length);
 		const targetDots = ceil(length / stepIncrement);
 
-		stroke(0);
+		
+		if (lineVectors.length > 1) {
+			closestPoint = getClosestPointOnLines(fieldMouse, lineVectors).vector;
 
-		if (1 < lineVectorList.length && lineVectorHasChanged) {
-			let dotCount = 0;
-			while (dotCount < targetDots) {
-				const newPoint = new Vector(previousPoint.x + xStepSize * (stepIncrement * dotCount), previousPoint.y + yStepSize * (stepIncrement * dotCount));
-				strokeWeight(8);
-				createNewPoint(newPoint);
-				dotCount += 1;
+			strokeWeight(12);
+			stroke(150);
+			point(closestPoint.toScreen().x, closestPoint.toScreen().y);
+
+			if (lineVectorHasChanged) {
+				for (let dotCount = 0; dotCount < targetDots; dotCount++) {
+					const newPoint = previousPoint.add(stepSize.mul(stepIncrement * dotCount));
+
+					stroke(0);
+					strokeWeight(8	);
+					createNewPoint(newPoint);
+				}
 			}
 		}
+
+		beginShape();
+		for (let i = 0; i < lineVectors.length; i++) {
+			const item = lineVectors[i];
+			vertex(item.toScreen().x, item.toScreen().y);
+
+			if (i === lineVectors.length - 1) {
+				stroke(150);
+				strokeWeight(2);
+				line(...lineVectors[i].toScreen(), ...mouseVector);
+			}
+		}
+		noFill();
+		stroke(0);
+		strokeWeight(2);
+		endShape();
+		
+		strokeWeight(5);
+		for (let j = 0; j < pointList.length; j++) {
+			if (j % visualizeNPoints === 0) {
+				point(
+					pointList[j].toScreen().x,
+					pointList[j].toScreen().y
+				);
+			}
+		}
+
+		for (const item of triggerPointList) {
+			stroke(0);
+			strokeWeight(18);
+			point(...item["vector"].toScreen());
+			strokeWeight(10);
+			stroke("#" + item["color"]);
+			point(...item["vector"].toScreen());
+		}
+		
 		lineVectorHasChanged = false;
 	}
 }
